8e1adb5e3c8cd55b6e47cb749cbd9434079e95ae
Brianllp <44490577+Brianllp@users.noreply.github.com>
2023-10-18 20:49:51 +0900

    add: サーバーを起動して hello world をブラウザで表示する

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,12 @@
 import express from "express";
 
 const app: express.Express = express();
+const port = 8000;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("Hello, Node.js!");
+});
+
+app.listen(port, () => {
+  console.log(`port ${port} でサーバー起動中`);
+});
